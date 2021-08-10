04862f1dc4ea01a55e947cf7a33b68763e0c49f4
Florent NOEL <florent.noel@protonmail.com>
2021-08-10 11:58:52 +0200

    Use Coil compose to load remote image

--- soli/app/src/main/java/com/heb/soli/HomeScreen.kt
+++ soli/app/src/main/java/com/heb/soli/HomeScreen.kt
@@ -1,24 +1,26 @@
 package com.heb.soli
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Tab
-import androidx.compose.material.TabPosition
-import androidx.compose.material.TabRow
+import androidx.compose.material.*
 import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import com.heb.soli.api.Media
 import com.heb.soli.api.MediaId
 import com.heb.soli.api.MediaType
@@ -35,9 +37,13 @@ fun HomePreview() {
             Media(MediaId(4), "FIP", "", MediaType.RADIO_STREAM),
         ),
         podcasts = listOf(
-            PodcastFeed(name = "afterhate", imageUrl = "", emptyList())
+            PodcastFeed(
+                name = "afterhate",
+                imageUrl = "https://www.afterhate.fr/wp-content/uploads/2018/03/Episode57-vignette.jpg",
+                emptyList()
+            )
         ),
-        selectedSection = HomeSection.Radio ,
+        selectedSection = HomeSection.Podcast,
         {}
     )
 }
@@ -48,7 +54,12 @@ fun HomeScreen(homeViewModel: HomeViewModel) {
     val podcasts = homeViewModel.podcasts.value
     val section = homeViewModel.selectedSection.value
 
-    HomeScreen(radios = radios, podcasts = podcasts, selectedSection = section, homeViewModel::onSectionSelected)
+    HomeScreen(
+        radios = radios,
+        podcasts = podcasts,
+        selectedSection = section,
+        homeViewModel::onSectionSelected
+    )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -115,24 +126,36 @@ fun PodcastList(medias: List<PodcastFeed>) {
     LazyVerticalGrid(
         cells = GridCells.Fixed(2)
     ) {
-        itemsIndexed(items = medias) { index, media ->
+        itemsIndexed(items = medias) { _, feed ->
             Column(
                 modifier = Modifier
                     .height(150.dp)
-                    .fillMaxSize()
-                    .background(Color.DarkGray),
+                    .fillMaxSize(),
                 verticalArrangement = Arrangement.Top,
             ) {
-                Text(
-                    media.name,
-                    Modifier
-                        .weight(1f)
-                        .padding(8.dp)
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 32.sp
-                )
+                if (feed.imageUrl.isEmpty()) {
+                    Text(
+                        feed.name,
+                        Modifier
+                            .weight(1f)
+                            .padding(8.dp)
+                            .fillMaxWidth()
+                            .wrapContentHeight(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 32.sp
+                    )
+                } else {
+                    Image(
+                        painter = rememberImagePainter(data = feed.imageUrl, builder = {
+                            placeholder(R.drawable.ic_launcher_background)
+                        }),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clip(MaterialTheme.shapes.medium),
+                    )
+                }
             }
         }
     }
