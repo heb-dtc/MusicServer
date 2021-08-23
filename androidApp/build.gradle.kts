c07465328ebed88128000f3c6649e15057b1f219
Florent NOEL <florent.noel@protonmail.com>
2021-08-23 17:15:50 +0200

    Update androidApp configuration

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,11 +11,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdk = 30
     defaultConfig {
         applicationId = "com.heb.soli.android"
-        minSdkVersion(21)
-        targetSdkVersion(30)
+        minSdk =21
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
     }
